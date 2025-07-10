f483dab0e73294dbd38d04a65413f9a90875cd3c
iggmonster <112781295+iggmonster@users.noreply.github.com>
2025-07-09 20:53:19 -0400

    Added server.py
    New server file to handle incoming queries from client.

---
+++ searchengine/server.py
@@ -0,0 +1,24 @@
+from flask import Flask, request, jsonify
+from query_engine import search
+
+app = Flask(__name__)
+
+@app.route("/search", methods=["GET"])
+def handle_search():
+    query = request.args.get("q")
+    use_raw = request.args.get("raw", "false").lower() == "true"
+    
+    if not query:
+        return jsonify({"error": "Missing query"}), 400
+    
+    results, original_query, corrected_query = search(query, use_raw_query=use_raw)
+    
+    return jsonify({
+        "original_query": original_query,
+        "corrected_query": corrected_query,
+        "used_raw_query": use_raw,
+        "results": results
+    })
+    
+if __name__ == "__main__":
+    app.run(debug=True)
